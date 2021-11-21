9f97d9011c2677035a0124017173ee946c2d872f
Jorge Hernández <jorge.hernandez1326@alumnos.udg.mx>
2021-11-21 16:02:41 -0600

    Funcionalidad de buscar hecha

---
+++ controllers/BD/buscar.php
@@ -0,0 +1,28 @@
+<?php
+
+include('conexion.php');
+$buscar=$_POST['busca'];
+
+if(!empty($buscar)){
+    $query="SELECT * FROM producto WHERE nom_producto LIKE '$buscar%'";
+    $result=mysqli_query($conexion,$query);
+    if(!$result){
+        die('Error'.mysqli_error($conexion));
+    }
+
+    $json = array();
+    while($fila = mysqli_fetch_array($result)){
+        $json[]= array(
+            "id" => $fila["id_producto"],
+            "nombre" => $fila["nom_producto"],
+            "precio" => $fila["precio"],
+            "imagen" => $fila["imagen"]
+        );
+    }
+    
+    $json_string = json_encode($json);
+
+    echo $json_string;
+}
+
+?>
--- controllers/app.js
+++ controllers/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+ 
+  function listarTodo(){
+    $.ajax({
+      url: "../controllers/BD/listarProductosIndex.php",
+      type: "GET",
+      success: function (response) {
+        let productos = JSON.parse(response);
+        let plantilla = "";
+        console.log(productos);
+        productos.forEach((producto) => {
+          plantilla += `
+                   <form method="POST" action = "./detalleProducto.html" class="formDetalle">
+          <article class="articulo">
+              <img src="${producto.imagen}" alt="esperanding">
+              <div class="nombre"> $ ${producto.nombre} </div>
+              <div class="precio"> $ ${producto.precio} </div>
+          </article>
+        </form> `;
+        });
+        $("#mainIndex").html(plantilla);
+      },
+    });
+  
+  }
+
+
+  listarTodo();
+
   $.ajax({
     url: "../controllers/BD/listarProductosIndex.php",
     type: "GET",
@@ -19,4 +47,59 @@ $(document).ready(function () {
       $("#mainIndex").html(plantilla);
     },
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("antes de empesar");
+  $('#buscar').keyup(function(e){
+  
+    console.log("Empece");
+    if($('#buscar').val()){
+      if(!($('#buscar').val()==="")){
+      let busca=$('#buscar').val();
+      $.ajax({
+        url:'../controllers/BD/buscar.php',
+        type: 'POST',
+        data: {busca},
+        success: function(response){
+          console.log(response);
+          let obj=JSON.parse(response);
+            let template='';
+            obj.forEach((objs)=>{
+              template+= `
+              <form method="POST" action = "./detalleProducto.html" class="formDetalle">
+     <article class="articulo">
+         <img src="${objs.imagen}" alt="esperanding">
+         <div class="nombre"> $ ${objs.nombre} </div>
+         <div class="precio"> $ ${objs.precio} </div>
+     </article>
+   </form> `;
+            });
+            $('#mainIndex').html(template);
+          }
+      })
+    }
+  }else{
+    listarTodo();
+  }
+    });
+
+    
+ 
 });
