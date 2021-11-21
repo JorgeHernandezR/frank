aec55c9831712e7d6cbe34038b9b0527ec519323
Jorge Hernández <jorge.hernandez1326@alumnos.udg.mx>
2021-11-21 15:01:58 -0600

    comenzar de nuevo

---
+++ controllers/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $.ajax({
+    url: "../controllers/BD/listarProductosIndex.php",
+    type: "GET",
+    success: function (response) {
+      let productos = JSON.parse(response);
+      let plantilla = "";
+      console.log(productos);
+      productos.forEach((producto) => {
+        plantilla += `
+                 <form method="POST" action = "./detalleProducto.html" class="formDetalle">
+        <article class="articulo">
+            <img src="${producto.imagen}" alt="esperanding">
+            <div class="nombre"> $ ${producto.nombre} </div>
+            <div class="precio"> $ ${producto.precio} </div>
+        </article>
+      </form> `;
+      });
+      $("#mainIndex").html(plantilla);
+    },
+  });
+});
