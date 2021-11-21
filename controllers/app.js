a1bb801c0876a745fb1d8b3fcb1c7b13632d2e6c
Jorge Hernández <jorge.hernandez1326@alumnos.udg.mx>
2021-11-21 17:38:41 -0600

    Modificacion de articulos

--- controllers/app.js
+++ controllers/app.js
@@ -15,6 +15,8 @@ $(document).ready(function () {
               <img src="${producto.imagen}" alt="esperanding">
               <div class="nombre"> $ ${producto.nombre} </div>
               <div class="precio"> $ ${producto.precio} </div>
+              <i class="fas fa-trash-alt"></i>
+              <i class="fas fa-pen"></i>
           </article>
         </form> `;
         });
