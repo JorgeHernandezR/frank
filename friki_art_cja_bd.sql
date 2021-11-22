3faedb96078c9410a721b5427b9429dda9e432bc
SalvadorMendozaB <SalvadorMen3@gmail.com>
2021-11-21 19:14:28 -0600

    Base de datos actualizada

--- friki_art_cja_bd.sql
+++ friki_art_cja_bd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2021 a las 22:59:57
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 22, 2021 at 01:13 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,45 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `friki_art_cja_bd`
+-- Database: `friki_art_cja_bd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `producto`
+-- Table structure for table `producto`
 --
 
-CREATE DATABASE friki_art_cja_bd;
-USE friki_art_cja_bd;
-
-CREATE TABLE `producto` (
-  `id_producto` int(11) NOT NULL,
+DROP TABLE IF EXISTS `producto`;
+CREATE TABLE IF NOT EXISTS `producto` (
+  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
   `nom_producto` varchar(40) NOT NULL,
-  `descripcion_producto` varchar(100) NOT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
   `precio` float NOT NULL,
-  `imagen` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `imagen` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_producto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tablas volcadas
+-- Dumping data for table `producto`
 --
 
---
--- Indices de la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id_producto`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `producto` (`id_producto`, `nom_producto`, `descripcion`, `precio`, `imagen`) VALUES
+(1, 'Videojuego Nier Automata PS4', 'Juego de accion y RPG japones', 1200, '../public/images/Nier.jpeg'),
+(2, 'Camiseta Yuno Mirai Niki ', 'Camiseta estampado anime ', 850, '../public/images/Camisa Mirai.jpeg'),
+(3, 'Maquillaje Kim Chic Palette ', 'Maquillaje para piel completa de la marca Kim Chic ', 1700, '../public/images/KinChi.jpeg'),
+(4, 'Funko-pop Hatsune Miku ', 'Funko-pop Hatsune miku Diva pop ', 700, '../public/images/FunkoMiku.jpeg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
